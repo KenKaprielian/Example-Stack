9892343f857774a388aa93558d390ba7af3b4d7e
Kenneth Kaprielian <30817280+KenKaprielian@users.noreply.github.com>
2022-11-16 12:26:11 -0500

    sqs to sns

--- main.tf
+++ main.tf
@@ -3,18 +3,15 @@ provider "aws" {
 }
 
 resource "aws_sns_topic" "sns-topic" {
-  provider     = "aws.sns"
   name         = "example-sns-topic"
   display_name = "example"
 }
 
 resource "aws_sqs_queue" "sqs-queue" {
-  provider = "aws.sqs"
   name     = "example-sqs-queue"
 }
 
 resource "aws_sns_topic_subscription" "sns-topic" {
-  provider  = "aws.sns2sqs"
   topic_arn = aws_sns_topic.sns-topic.arn
   protocol  = "sqs"
   endpoint  = aws_sqs_queue.sqs-queue.arn
