378ee6b7ab50c46d0acad746bb5e399f80b32c79
Kenneth Kaprielian <30817280+KenKaprielian@users.noreply.github.com>
2022-11-16 12:23:30 -0500

    sns and sqs

--- main.tf
+++ main.tf
@@ -2,12 +2,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_sqs_queue" "terraform_queue" {
-  name                        = "terraform-example-queue.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
+resource "aws_sns_topic" "sns-topic" {
+  provider     = "aws.sns"
+  name         = "example-sns-topic"
+  display_name = "example"
 }
 
-resource "aws_sns_topic" "user_updates" {
-  name = "user-updates-topic"
+resource "aws_sqs_queue" "sqs-queue" {
+  provider = "aws.sqs"
+  name     = "example-sqs-queue"
+}
+
+resource "aws_sns_topic_subscription" "sns-topic" {
+  provider  = "aws.sns2sqs"
+  topic_arn = aws_sns_topic.sns-topic.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.sqs-queue.arn
 }
