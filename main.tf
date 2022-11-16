aa32117f6ce814664d5ef7aa5e0d52c6efd14aa1
Kenneth Kaprielian <30817280+KenKaprielian@users.noreply.github.com>
2022-11-16 11:39:39 -0500

    fifo que

--- main.tf
+++ main.tf
@@ -3,17 +3,7 @@ provider "aws" {
 }
 
 resource "aws_sqs_queue" "terraform_queue" {
-  name                      = "terraform-example-queue"
-  delay_seconds             = 90
-  max_message_size          = 2048
-  message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.terraform_queue_deadletter.arn
-    maxReceiveCount     = 4
-  })
-
-  tags = {
-    Environment = "production"
-  }
+  name                        = "terraform-example-queue.fifo"
+  fifo_queue                  = true
+  content_based_deduplication = true
 }
